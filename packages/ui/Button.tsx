ac18be861a3a2fdbd6e835c978057c021018e692
maruk0chan <marco.chan@penqe.com>
2023-08-10 13:35:29 +0900

    use ThemeProvider

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import * as React from "react";
-import { styled } from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 
 const StyledButton = styled.button`
   background-color: white;
-  padding: 10px 20px
+  padding: 10px 20px;
+  color: ${props => props.theme.color};
 `
 
 export const Button = () => {
-  return <StyledButton onClick={() => alert("default")}>Default Button</StyledButton>;
+  return <ThemeProvider theme={{ color: 'yellow' }}>
+    <StyledButton onClick={() => alert("default")}>Default Button</StyledButton></ThemeProvider>;
 };
